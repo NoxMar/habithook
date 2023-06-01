bd2c8d3eec77899d28a4bb9332fa9f46039a59c1
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-06-01 15:38:50 +0200

    fix(ui): dodaj BlazorBootstrap do kontenera DI
    Wcześniejsze pomninięce tego kroku powodowało błędne działanie i wyjątki w komponentach z pakietu `Blazor.Bootstrap`.

--- src/HabitHook/HabitHook.HabitManagement.Client/Program.cs
+++ src/HabitHook/HabitHook.HabitManagement.Client/Program.cs
@@ -1,3 +1,4 @@
+using BlazorBootstrap;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using HabitHook.HabitManagement.Client;
@@ -7,6 +8,8 @@ using HabitHook.HabitManagement.Domain.Contracts.Habits.AddHabit;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddBlazorBootstrap();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
